653e103f7956062e92763a9da87a37cb3bf21272
WinVit <teploton.ru@mail.ru>
2024-09-16 16:03:59 +0300

    160924 изменения

--- mobilization.py
+++ mobilization.py
@@ -3,100 +3,87 @@ import PySimpleGUI as sg
 import keypad
 import logic
 
+
 def main():
 
     sg.theme('Dark')
 
-    layout = [
-              [sg.Text('Проба ввода с Keypad', expand_x=True), sg.Button('\U000026A0\U00002692', s=(1, 1), button_color='grey'), sg.Button('\U0000260F', s=(1, 1), button_color='blue'), sg.Button('\U00002716', s=(1, 1), button_color='red'), ],
-              [sg.Button('Доходы (+)',
-              expand_x=True,
-              #s=(0, 0),
-              ),
-              sg.Button('Расходы (-)',
-              expand_x=True,
-              ),
-              ],
-              [sg.Frame('Суточные', 
-              [[sg.Button('Оплата суток',
-              expand_x=True,
-              )
-              ],
-              [sg.Text('0.0 ₽', expand_x=True, font=('Helvetica', 8, 'italic'), text_color='red', key='-SUT-')]],
-              title_color='pink',
-              font=('Helvetica', 10, 'bold'),
-              #expand_x=True,
-              ),
-              #],
-              #[
-              sg.Frame('Аренда жилья', 
-              [[sg.Button('Оплата, р/ч.д.',
-              expand_x=True,
-              )
-              ],
-              [sg.Text('0.0 ₽', expand_x=True, font=('Helvetica', 8, 'italic'), text_color='red', key='-RENT-')]],
-              title_color='pink',
-              font=('Helvetica', 10, 'bold'),
-              #expand_x=True,
-              )],
-              [sg.Frame('Остаток (=)', [[sg.Text('0.0 ₽', expand_x=True, font=('Helvetica', 14, 'italic'), text_color='red', key='-OUT-')], [sg.Multiline('Общие данные', font='Courier 7', text_color='red', background_color='lightblue',justification='center', expand_x=True, disabled=False, no_scrollbar=True, size=(0, 1)), ], ], font=('Helvetica', 14, 'bold'), expand_x=True)],
+    k_sut = 2.11
+    k_rent = 1.4
+    k_staffTravel = 1.4
+
+    layout_V_E_S = sg.Button('Объем работ\n0.0 м2(шт, м.п.)', expand_x=True, s=(0, 2)), sg.Button('Выработка\n0.0 м2(шт, м.п.)/ч.д.', expand_x=True, s=(0, 2)), sg.Button('Персонал рабочих\n0 чел.', expand_x=True, s=(0, 2))
+    layout_sut = sg.Frame('Суточные',
+                          [[sg.Button('Оплата чел. в сутки\n0.0 р/ч.д.', expand_x=True, s=(0, 2), key='-T_SUT-'),],
+                           [sg.Text('0.0 ₽', expand_x=True,
+                                    text_color='red', key='-SUT-')],
+                           ],
+                          title_color='pink',
+                          expand_x=True,
+                          )
+    layout_rent = sg.Frame('Аренда жилья',
+                           [[sg.Button('Оплата за чел. в сутки\n0.0 р/ч.д.', expand_x=True, s=(0, 2)),],
+                            [sg.Text('0.0 ₽', expand_x=True,
+                                     text_color='red', key='-RENT-')],
+                            ],
+                           title_color='pink',
+                           expand_x=True,
+                           )
+
+    layout_staff_travel = sg.Frame('Проезд персонала (1 поездка в 2 месяца)',
+                                   [[sg.Button('Оплата чел. в 1 конец\n0.0 р/ч.д.', expand_x=True, s=(0, 2)), sg.Button('Кол-во поездок\n0.0 шт', expand_x=True, s=(0, 2)),],
+                                    [sg.Text('0.0 ₽', expand_x=True,
+                                             text_color='red', key='-STAFFTRAVEL-')],
+                                    ],
+                                   title_color='pink',
+                                   expand_x=True,
+                                   )
+
+    layout = [[layout_V_E_S],
+              [layout_sut, layout_rent,],
+              [layout_staff_travel],
               ]
-    
-    window = sg.Window('MAIN_window', layout,
-                       #default_button_element_size=(14, 2),
-                       font=('Helvetica', 5, 'italic'),
-                       auto_size_buttons=False,
-                       #modal = True,
-                       finalize = True,
+
+    window = sg.Window('Мобилизационные расходы',
+                       layout,
+                       auto_size_buttons=True,
+                       modal = True,
+                       finalize=True,
                        )
-    window.Maximize()
-    
-    
+    # window.Maximize()
+
     while True:
-        
         event, values = window.read()
-        
         if event in (sg.WIN_CLOSED, '\U00002716'):
             break
-    
-        elif event == 'Доходы (+)':
-            profit = logic.Profit(keypad.keypad()).profit
-            window['Доходы (+)'].update(f'Доходы (+)\n{round(float(profit),2)} ₽')
-            
-        elif event == 'Расходы (-)':
-            debet = logic.Debet(keypad.keypad()).debet
-            window['Расходы (-)'].update(f'Расходы (-)\n{round(float(debet),2)} ₽')
-            
-        elif event == 'Оплата суток':
+        elif event == '-T_SUT-':
             sut = logic.Sut(keypad.keypad()).sut
-            window['Оплата суток'].update(f'Оплата суток\n{round(float(sut),2)} ₽/ч.д.')
-            
-        result = logic.Result().result()
-        window['-OUT-'].update(f'{round(float(result),2)} ₽')
-    
+            window['-T_SUT-'].update(
+                f'Оплата чел. в сутки\n{round(float(sut),2)} ₽/ч.д.')
+            window['-SUT-'].update(f'Всего: {round(float(sut*k_sut),2)} ₽')
     window.close()
-    
-    
+
+
 if __name__ == '__main__':
-    
+
     main()
-    
-    
-#диаметр#\U000000F8
-#череп#\U00002620
-#пиш.рука#\U0000270D (Е
-#конверт#\U00002709
-#x#\U00002716 (2718, 2573, 2612, 2327)
-#указ.палец#\U0000261A (B, C, E, F)
-#внимание#\U000026A0
-#настройки#\U00002692
-#телефон#\U0000260F (E, 2121, 2706, )
-#собачка#\U00000040
-#стрелка ввода#\U000023CE (21B2
-#рециклинг#\U00002672 (267B
-#галочка#\U00002713 (2714, 2611
-#лупа#\U00002315
-#ножницы#\U00002704 (2702
-#стрелки туда-сюда#\U000021C6
-#очистить, удалить#\U0000232B
-#обновить#\U000021BB
+
+
+# диаметр#\U000000F8
+# череп#\U00002620
+# пиш.рука#\U0000270D (Е
+# конверт#\U00002709
+# x#\U00002716 (2718, 2573, 2612, 2327)
+# указ.палец#\U0000261A (B, C, E, F)
+# внимание#\U000026A0
+# настройки#\U00002692
+# телефон#\U0000260F (E, 2121, 2706, )
+# собачка#\U00000040
+# стрелка ввода#\U000023CE (21B2
+# рециклинг#\U00002672 (267B
+# галочка#\U00002713 (2714, 2611
+# лупа#\U00002315
+# ножницы#\U00002704 (2702
+# стрелки туда-сюда#\U000021C6
+# очистить, удалить#\U0000232B
+# обновить#\U000021BB
